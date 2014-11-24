b672d242f7d058e943f16b308791b3b2ef69e915
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-11-24 12:56:39 -0800

    Adding lookback.io

--- Pods/Target Support Files/Pods-Polldaddy/Pods-Polldaddy-environment.h
+++ Pods/Target Support Files/Pods-Polldaddy/Pods-Polldaddy-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_HockeySDK 6
 #define COCOAPODS_VERSION_PATCH_HockeySDK 1
 
+// Lookback
+#define COCOAPODS_POD_AVAILABLE_Lookback
+#define COCOAPODS_VERSION_MAJOR_Lookback 0
+#define COCOAPODS_VERSION_MINOR_Lookback 6
+#define COCOAPODS_VERSION_PATCH_Lookback 5
+
 // Mixpanel
 #define COCOAPODS_POD_AVAILABLE_Mixpanel
 #define COCOAPODS_VERSION_MAJOR_Mixpanel 2
